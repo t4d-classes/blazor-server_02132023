750e2e1474105bf9a9b1407a6159cf0d42b9d1ed
Eric Greene <eric@t4d.io>
2023-02-14 18:59:42 -0500

    add automapper code

---
+++ ToolsApp/ToolsApp.Components/CarTool/Models/CarForm.cs
@@ -0,0 +1,10 @@
+﻿namespace ToolsApp.Components.CarTool.Models;
+
+public class CarForm
+{
+  public string Make { get; set; }
+  public string Model { get; set; }
+  public int Year { get; set; }
+  public string Color { get; set; }
+  public decimal Price { get; set; }
+}
---
+++ ToolsApp/ToolsApp.Components/CarTool/Models/CarToolMapper.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using ToolsApp.Models;
+using ToolsApp.Core.Interfaces.Models;
+
+namespace ToolsApp.Components.CarTool.Models;
+
+public class CarToolMapper
+{
+  public static MapperConfiguration GetConfig() {
+    return new MapperConfiguration(config =>
+    {
+      config.CreateMap<CarForm, NewCar>();
+      config.CreateMap<ICar, CarForm>();
+      config.CreateMap<CarForm, Car>();
+    });
+  }
+
+  public static IMapper GetMapper() {
+    return GetConfig().CreateMapper();
+  }
+}
