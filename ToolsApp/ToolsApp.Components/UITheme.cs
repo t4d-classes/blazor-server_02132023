36f2df1d6f48689ec58b6f4cd89e65a95b5e9259
Eric Greene <eric@t4d.io>
2023-02-16 18:28:47 -0500

    add cascading ui theme

---
+++ ToolsApp/ToolsApp.Components/UITheme.cs
@@ -0,0 +1,13 @@
+﻿namespace ToolsApp.Components;
+
+public class ButtonStyle {
+  public string BackgroundColor {  get; set; } = "initial";
+  public string TextColor { get; set; } = "initial";
+}
+
+public class UITheme
+{
+  public ButtonStyle? PrimaryButton { get; set; }
+  public ButtonStyle? SecondaryButton { get; set; }
+  public ButtonStyle? DangerButton { get; set; }
+}
