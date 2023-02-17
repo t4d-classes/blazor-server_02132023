fad64b0cee214b0c138b6060602bca7684723ed5
Eric Greene <eric@t4d.io>
2023-02-17 13:41:35 -0500

    Add solution to exercise 15

--- ToolsApp/ToolsApp.Components/CarTool/Models/CarForm.cs
+++ ToolsApp/ToolsApp.Components/CarTool/Models/CarForm.cs
@@ -1,10 +1,22 @@
-﻿namespace ToolsApp.Components.CarTool.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToolsApp.Components.CarTool.Models;
 
 public class CarForm
 {
+  [Required]
+  [MinLength(3)]
   public string Make { get; set; }
+  
+  [Required]
   public string Model { get; set; }
+
+  [Required] 
   public int Year { get; set; }
+
+  [Required] 
   public string Color { get; set; }
+
+  [Required] 
   public decimal Price { get; set; }
 }
